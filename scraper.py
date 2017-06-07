0a9bc492e4f6211a59aca2e4b158d9a3be689b97
blablupcom <o.volodin@yahoo.com>
2017-06-07 18:58:20 +0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,9 +9,7 @@ import urllib2
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-
-#### FUNCTIONS 1.1
-import requests   #import requests for validating urls
+#### FUNCTIONS 1.0
 
 def validateFilename(filename):
     filenameregex = '^[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[0-9][0-9][0-9][0-9]_[0-9QY][0-9]$'
@@ -39,19 +37,19 @@ def validateFilename(filename):
 
 def validateURL(url):
     try:
-        r = requests.get(url)
+        r = urllib2.urlopen(url)
         count = 1
-        while r.status_code == 500 and count < 4:
+        while r.getcode() == 500 and count < 4:
             print ("Attempt {0} - Status code: {1}. Retrying.".format(count, r.status_code))
             count += 1
-            r = requests.get(url)
+            r = urllib2.urlopen(url)
         sourceFilename = r.headers.get('Content-Disposition')
 
         if sourceFilename:
             ext = os.path.splitext(sourceFilename)[1].replace('"', '').replace(';', '').replace(' ', '')
         else:
             ext = os.path.splitext(url)[1]
-        validURL = r.status_code == 200
+        validURL = r.getcode() == 200
         validFiletype = ext.lower() in ['.csv', '.xls', '.zip', '.xlsx', '.pdf']
         return validURL, validFiletype
     except:
@@ -86,8 +84,8 @@ def convert_mth_strings ( mth_string ):
 
 #### VARIABLES 1.0
 
-entity_id = "NHTRTVFT_5BPNFT_gov"
-url = "http://www.5boroughspartnership.nhs.uk/financial-transparency-reports/"
+entity_id = "FTRXLX_BTHNFT_gov"
+url = "http://www.bfwh.nhs.uk/about-our-trust/transparency/"
 errors = 0
 data = []
 
@@ -100,13 +98,26 @@ soup = BeautifulSoup(html, 'lxml')
 #### SCRAPE DATA
 
 
-blocks = soup.find('div', 'related_docs').find_all('a')
+blocks = soup.find_all('div', 'document-icon')
 for block in blocks:
-    url = 'http://www.5boroughspartnership.nhs.uk' + block['href']
-    csvMth = block.text.split()[0][:3]
-    csvYr = block.text.split()[-1]
+    link = block.find('a')
+    url = link['href']
+    csvMth = link.find('span').text.split('-')[0][:3]
+    csvYr = link.find('span').text.split('-')[-1]
     csvMth = convert_mth_strings(csvMth.upper())
     data.append([csvYr, csvMth, url])
+page_link = soup.find('a', attrs={'rel':'noopener noreferrer'})['href']
+html = urllib2.urlopen(page_link)
+soup_page = BeautifulSoup(html, 'lxml')
+blocks_doc = soup_page.find_all('div', 'col-sm-6')
+for block_doc in blocks_doc:
+    csvMth = block_doc.find('span', 'inner-cell').text.split()[0][:3]
+    csvYr = block_doc.find('span', 'inner-cell').text.split()[1]
+    doc_link = block_doc.find_all('a')[-1]['href']
+    csvMth = convert_mth_strings(csvMth.upper())
+    data.append([csvYr, csvMth, url])
+
+
 
 
 #### STORE DATA 1.0
